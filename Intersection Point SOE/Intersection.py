d513af9d7e3c6d6a69400e04619d72b32806256c
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2025-01-15 18:40:49 -0500

    Update Intersection.py

--- Intersection Point SOE/Intersection.py
+++ Intersection Point SOE/Intersection.py
@@ -1,1 +1,11 @@
 # given coordinates of vector, output the coordinates of where the vector lands
+def intersection(iHat, jHat, result):
+  if len(result) != len(iHat) or len(result) != len(jHat):
+    return "Not a square matrix"
+  else:
+    for i in range(len(iHat)):
+      result[i] = result[i]*iHat[i]
+      for j in range(len(jHat)):
+        result[j] = result[j]*jHat[j]
+    
+  
